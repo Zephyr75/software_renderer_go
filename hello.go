92b7f9ec798d9f90d3398e79a362c59c3ffbcae3
Zéphyr <antoine.familleroger@gmail.com>
2022-08-28 01:32:51 +0200

    Added settings

--- hello.go
+++ hello.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/canvas"
+	"overdrive/test"
+	"fmt"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello World")
+	myApp := app.New()
+	w := myApp.NewWindow("Line")
 
-	w.SetContent(widget.NewLabel("Hello World!"))
+	line := canvas.NewLine(color.White)
+	line.StrokeWidth = 1
+	w.SetContent(line)
+
+	w.Resize(fyne.NewSize(100, 100))
 	w.ShowAndRun()
-}
 
+
+	fmt.Println(test.ReverseRunes("!oG ,olleH"))
+}
---
+++ utilities/settings.go
@@ -0,0 +1,11 @@
+package utilities
+//import pi and use it
+import "math"
+
+const FOV = 45
+const RESOLUTION_X = 1000
+const RESOLUTION_Y = 600
+
+func z0() float64 {
+	return (RESOLUTION_X / 2) / math.Tan((FOV / 2) * math.Pi / 180)
+}
