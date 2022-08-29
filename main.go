ab79e9ef751caf7f7a4d6d0628314f9c81b6be28
Zéphyr <antoine.familleroger@gmail.com>
2022-08-29 12:29:16 +0200

    Fix display

--- main.go
+++ main.go
@@ -1,35 +1,32 @@
 package main
 
 import (
-	"fmt"
-	"image"
 	"image/color"
+	"image"
 
-	//"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
-
-	//"fyne.io/fyne/v2/theme"
-
-	"overdrive/geometry"
-	"overdrive/mesh"
-	"overdrive/render"
 	"overdrive/utilities"
+	"overdrive/mesh"
+	"overdrive/geometry"
+	"overdrive/render"
+	
+	"fmt"
 	"time"
 )
 
 func main() {
-
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Overdrive")
+	myWindow := myApp.NewWindow("Canvas")
 	myCanvas := myWindow.Canvas()
 
 	src := image.NewRGBA(image.Rect(0, 0, utilities.RESOLUTION_X, utilities.RESOLUTION_Y))
-	img := canvas.NewImageFromImage(src)
-	myCanvas.SetContent(img)
+	rect := canvas.NewImageFromImage(src)
+	myCanvas.SetContent(rect)
 
 	go func() {
+
 		cube := mesh.Cube(geometry.VectorNew(100, 0, 0), geometry.VectorNew(100, 0, 0), geometry.VectorNew(400, 400, 400))
 		cam := render.Camera{
 			Position: geometry.VectorZero(), 
@@ -41,23 +38,34 @@ func main() {
 			Color: color.White,
 			Length: 0,
 		}
-	
-		start := time.Now()
-		src = image.NewRGBA(image.Rect(0, 0, utilities.RESOLUTION_X, utilities.RESOLUTION_Y))
 
-		for i := 0; i < 10000; i++ {
+		start := time.Now()
+
+		for {
+			// for x := 0; x < utilities.RESOLUTION_X; x++ {
+			// 	for y := 0; y < utilities.RESOLUTION_Y; y++ {
+			// 		src.Set(x, y, color.Black)
+			// 	}
+			// }
+
+			src = image.NewRGBA(image.Rect(0, 0, utilities.RESOLUTION_X, utilities.RESOLUTION_Y))
+
 			cube.Draw(src, &cam, []render.Light{light})
 			cube.Translate(geometry.VectorNew(0, 1, 0))
-			img = canvas.NewImageFromImage(src)
-			img.FillMode = canvas.ImageFillOriginal
-			//img.FillColor = green
-			img.Refresh()
-			
-			fmt.Println("fps: ", 1000 / time.Since(start).Milliseconds())
+			cube.Rotate(geometry.VectorNew(0, 0.01, 0))
+
+			rect.Image = src
+			rect.Refresh()
+
+			t := time.Since(start).Milliseconds()
+			if t == 0 {
+				t = 1
+			}
+			fmt.Println("fps: ", 1000 / t)
 			start = time.Now()
 		}
 	}()
 
-	myWindow.Resize(fyne.NewSize(1000, 500))
+	myWindow.Resize(fyne.NewSize(utilities.RESOLUTION_X, utilities.RESOLUTION_Y))
 	myWindow.ShowAndRun()
 }
